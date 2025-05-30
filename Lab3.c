e0e0e7a2229e9414298851785c8b13cbbc01dfdd
MihailMiet <studymiet0@outlook.com>
2025-05-30 15:55:47 +0300

    Lab3_v2

--- Lab3.c
+++ Lab3.c
@@ -7,13 +7,15 @@ int main(void) {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 	int length=0, height=0, i=0, lcount=0, hcount=0, mem=0;
-	while (i <= length) { printf("-"); i++; }
-	i = 0;
+	int *matmem_l, **M;
 	while (1) {
 		lcount = 0, hcount = 0;
-		printf("Enter height_length");
+		printf("Enter height_length\n");
 		scanf_s("%d %d", &height, &length);
-		int M[height][length];
+		while (i <= length) { printf("- "); i++; }
+		i = 0;
+		matmem_l = malloc(sizeof(int) * length);
+		M = calloc(height, matmem_l);
 		while (hcount < height) {
 			while (lcount < length) { 
 				M[lcount][hcount] = rand() % 100;  
@@ -24,10 +26,10 @@ int main(void) {
 		lcount = 0, hcount = 0;
 		while (hcount < height) {
 			while (lcount < length-1) {
-				printf("%d", M[hcount][lcount]);
+				printf("%d ", M[hcount][lcount]);
 				lcount++;
 			}
-			printf("%d\n", M[hcount][lcount + 1]);
+			printf("%d \n", M[hcount][lcount + 1]);
 			hcount++;
 		}
 		lcount = 0, hcount = 0;
@@ -42,11 +44,15 @@ int main(void) {
 		lcount = 0, hcount = 0;
 		while (hcount < height) {
 			while (lcount < length) { 
-				printf("%d", M[hcount][lcount]);
+				printf("%d ", M[hcount][lcount]);
 				lcount++;
 			}
-			printf("%d\n", M[hcount][lcount+1]);
+			printf("%d \n", M[hcount][lcount+1]);
 			hcount++;
-		}	
+
+		}
+		free(M);
+		free(matmem_l);
 	}
+	return 0;
 }
